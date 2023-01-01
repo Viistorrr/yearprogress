dbd521158bd6286b9a3cd1e04d122542757c808f
viistorrr <victormeza41@gmail.com>
2023-01-01 14:46:34 -0500

    year progress

---
+++ app/page.tsx
@@ -0,0 +1,81 @@
+const TOTAL_DAYS = 365;
+const TOTAL_MONTHS = 12;
+const TOTAL_HOURS = 24;
+
+export default function Home() {
+  const year = new Date().getFullYear();
+  const month = new Date().getMonth();
+  const date = new Date().getDate();
+  const hours = new Date().getHours();
+
+  const YearPercent = (date / TOTAL_DAYS) * 100;
+  const MonthPercent = (date / TOTAL_MONTHS) * 100;
+  const DayPercent = (hours / TOTAL_HOURS) * 100;
+
+  return (
+    <main className="flex flex-col justify-center items-center p-10 min-h-screen">
+      <div className="flex flex-col w-1/2 items-center justify-center">
+        <h1 className="text-3xl font-bold text-slate-700">
+          {date}/{month + 1}/{year}
+        </h1>
+        <div className="flex w-full items-center align-center">
+          <h1 className="font-bold w-2/12 py-4">Año </h1>
+          <div className="w-8/12 bg-gray-300 rounded-full h-4 dark:bg-gray-700">
+            <div
+              className="bg-sky-900 h-4 rounded-full"
+              style={{ width: "1%" }}
+            ></div>
+          </div>
+          <h1 className="font-bold w-2/12 px-4">{YearPercent.toFixed(2)} % </h1>
+        </div>
+        <div className="flex w-full items-center align-center">
+          <h1 className="font-bold w-2/12 py-4">Mes </h1>
+          <div className="w-8/12 bg-gray-300 rounded-full h-4 dark:bg-gray-700">
+            <div
+              className="bg-sky-700 h-4 rounded-full"
+              style={{ width: "1%" }}
+            ></div>
+          </div>
+          <h1 className="font-bold w-2/12 px-4">
+            {MonthPercent.toFixed(2)} %{" "}
+          </h1>
+        </div>
+        <div className="flex w-full items-center align-center">
+          <h1 className="font-bold w-2/12 py-4">Día </h1>
+          <div className="w-8/12 bg-gray-300 rounded-full h-4 dark:bg-gray-700">
+            <div
+              className="bg-sky-500 h-4 rounded-full"
+              style={{ width: "1%" }}
+            ></div>
+          </div>
+          <h1 className="font-bold w-2/12 px-4">{DayPercent.toFixed(2)} % </h1>
+        </div>
+      </div>
+      <div>
+        <p className="text-sm text-slate-700">
+          Hecho por{" "}
+          <a
+            className="text-sky-900 hover:text-sky-700"
+            href="https://twitter.com/viistorrr"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <b>@viistorrr</b>
+          </a>
+        </p>
+      </div>
+      <div>
+        <p className="text-sm text-slate-700 py-4">
+          <a
+            className="text-sky-900 hover:text-sky-700"
+            href="https://www.viistorrr.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            www.viistorrr.com
+          </a>
+        </p>
+      </div>
+    </main>
+  );
+}
