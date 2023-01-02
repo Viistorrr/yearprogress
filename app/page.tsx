4f2a123ef2bba624596443a4e4a09b4d5a723976
viistorrr <victormeza41@gmail.com>
2023-01-02 12:02:51 -0500

    week info and % labels updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { LocalDateTime } from "local-date";
 
 const TOTAL_DAYS = 365;
+const TOTAL_WEEK_DAYS = 7;
 const TOTAL_MONTHS = 12;
 const TOTAL_HOURS = 24;
 
@@ -10,10 +11,12 @@ export default function Home() {
   const date = new Date().getDate();
   const hours = new Date().getHours();
   const minutes = new Date().getMinutes();
+  const weekDay = new Date().getDay();
 
   const YearPercent = (date / TOTAL_DAYS) * 100;
   const MonthPercent = (date / TOTAL_MONTHS) * 100;
   const DayPercent = (hours / TOTAL_HOURS) * 100;
+  const WeekPercent = (weekDay / TOTAL_WEEK_DAYS) * 100;
 
   var now: any = new Date();
   var start: any = new Date(now.getFullYear(), 0, 0);
@@ -24,7 +27,7 @@ export default function Home() {
   console.log();
 
   return (
-    <main className="flex flex-col justify-center items-center p-10 min-h-screen">
+    <main className="flex flex-col justify-start items-center p-10 min-h-screen">
       <div className="flex flex-col w-full lg:w-1/2 items-center justify-center">
         <h1 className="text-3xl font-bold text-slate-700 p-16">
           Día {day} de {TOTAL_DAYS}
@@ -36,34 +39,54 @@ export default function Home() {
           <h1 className="font-bold w-2/12 py-4">Día </h1>
           <div className="w-8/12 bg-gray-300 rounded-full h-4 dark:bg-gray-700">
             <div
-              className="bg-sky-500 h-4 rounded-full"
+              className="flex items-center justify-center py-2 bg-sky-900 h-4 rounded-full"
               style={{ width: DayPercent.toString() + "%" }}
-            ></div>
+            >
+              <h1 className="font-bold items-center text-sm text-white">
+                {DayPercent.toFixed(2)} %{" "}
+              </h1>
+            </div>
           </div>
-          <h1 className="font-bold w-2/12 px-4">{DayPercent.toFixed(2)} % </h1>
+        </div>
+        <div className="flex w-full items-center align-center">
+          <h1 className="font-bold w-2/12 py-4">Semana </h1>
+          <div className="w-8/12 bg-gray-300 rounded-full h-4 dark:bg-gray-700">
+            <div
+              className="flex items-center justify-center py-2 bg-sky-700 h-4 rounded-full"
+              style={{ width: WeekPercent.toString() + "%" }}
+            >
+              <h1 className="font-bold items-center text-sm text-white">
+                {WeekPercent.toFixed(2)} %{" "}
+              </h1>
+            </div>
+          </div>
         </div>
         <div className="flex w-full items-center align-center">
           <h1 className="font-bold w-2/12 py-4">Mes </h1>
           <div className="w-8/12 bg-gray-300 rounded-full h-4 dark:bg-gray-700">
             <div
-              className="bg-sky-700 h-4 rounded-full"
+              className="flex items-center justify-center py-2 bg-sky-500 h-4 rounded-full"
               style={{ width: MonthPercent.toString() + "%" }}
-            ></div>
+            >
+              <h1 className="font-bold items-center text-sm text-white">
+                {MonthPercent.toFixed(2)} %{" "}
+              </h1>
+            </div>
           </div>
-          <h1 className="font-bold w-2/12 px-4">
-            {MonthPercent.toFixed(2)} %{" "}
-          </h1>
         </div>
 
         <div className="flex w-full items-center align-center">
           <h1 className="font-bold w-2/12 py-4">Año </h1>
           <div className="w-8/12 bg-gray-300 rounded-full h-4 dark:bg-gray-700">
             <div
-              className="bg-sky-900 h-4 rounded-full"
+              className="flex items-center justify-center py-2 bg-sky-500 h-4 rounded-full"
               style={{ width: YearPercent.toString() + "%" }}
-            ></div>
+            >
+              <h1 className="font-bold text-sm text-sky-900">
+                {YearPercent.toFixed(2)} %{" "}
+              </h1>
+            </div>
           </div>
-          <h1 className="font-bold w-2/12 px-4">{YearPercent.toFixed(2)} % </h1>
         </div>
       </div>
       <div>
