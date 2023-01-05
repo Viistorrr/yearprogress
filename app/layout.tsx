a80f006c6529accaff1b4ee83192006e3a0b0fe2
viistorrr <victormeza41@gmail.com>
2023-01-04 21:52:24 -0500

    trying to fix bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,13 +7,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark" data-theme="dark">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body className="dark">{children}</body>
     </html>
   );
 }
