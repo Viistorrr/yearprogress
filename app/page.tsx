7c5c5ff15ac58041083faf9e18fcd489ab3ea35b
viistorrr <victormeza41@gmail.com>
2023-01-04 21:26:20 -0500

    trying to fix bug

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default async function Home() {
   const utcOffset = localTimeData.utc_offset;
   const utcOffsetHours = localTimeData.utc_offset;
   const date = globalDate.getDate();
-  var hours = globalDate.getHours() + parseInt(utcOffsetHours);
+  var hours = globalDate.getHours() + parseInt(utcOffsetHours) + 5;
   if (hours == 19) hours = 7;
   if (hours == 20) hours = 8;
   if (hours == 21) hours = 9;
