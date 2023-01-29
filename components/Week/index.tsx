db2e1f32711f4570df100903d026e3dac15eb818
viistorrr <victormeza41@gmail.com>
2023-01-29 06:59:45 -0500

    fixing show days

--- components/Week/index.tsx
+++ components/Week/index.tsx
@@ -2,26 +2,26 @@ const Week = ({weekDay}:any) => {
 
  
   return (
-    <div className="flex flex-wrap items-center py-4">
-        <div className={`px-6 rounded-md border-2 border-gray-800 ${weekDay === 7 ? 'font-bold':''}`}>
+    <div className="flex flex-wrap items-center py-2">
+        <div className={`px-6 ${weekDay === 7 ? 'font-bold bg-emerald-200 rounded-full border-2 border-emerald-500':''}`}>
          Domingo
         </div>
-        <div className={`px-6 rounded-md border-2 border-gray-800 ${weekDay === 1 ? 'font-bold':''}`}>
+        <div className={`px-6 ${weekDay === 1 ? 'font-bold bg-emerald-200 rounded-full border-2 border-emerald-400':''}`}>
             Lunes
         </div>
-        <div className={`px-6 rounded-md border-2 border-gray-800 ${weekDay === 2 ? 'font-bold':''}`}>
+        <div className={`px-6 ${weekDay === 2 ? 'font-bold bg-emerald-200 rounded-full border-2 border-emerald-400':''}`}>
             Martes
         </div>
-        <div className={`px-6 rounded-md border-2 border-gray-800 ${weekDay === 3 ? 'font-bold':''}`}>
+        <div className={`px-6 ${weekDay === 3 ? 'font-bold bg-emerald-200 rounded-full border-2 border-emerald-400':''}`}>
             Miércoles
         </div>
-        <div className={`px-6 rounded-md border-2 border-gray-800 ${weekDay === 4 ? 'font-bold':''}`}>
+        <div className={`px-6 ${weekDay === 4 ? 'font-bold bg-emerald-200 rounded-full border-2 border-emerald-400':''}`}>
             Jueves
         </div>
-        <div className={`px-6 rounded-md border-2 border-gray-800 ${weekDay === 5 ? 'font-bold':''}`}>
+        <div className={`px-6 ${weekDay === 5 ? 'font-bold bg-emerald-200 rounded-full border-2 border-emerald-400':''}`}>
             Viernes
         </div>
-        <div className={`px-6 rounded-md border-2 border-gray-800 ${weekDay === 6 ? 'font-bold':''}`}>
+        <div className={`px-6 ${weekDay === 6 ? 'font-bold bg-emerald-200 rounded-full border-2 border-emerald-400':''}`}>
             Sábado
         </div>
     </div>
