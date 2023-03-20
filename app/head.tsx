d347cbd9f3b8456b5ddb2920620ecb1df93efab9
viistorrr <victormeza41@gmail.com>
2023-03-20 09:07:00 -0500

    fixing opengrapg desc

--- app/head.tsx
+++ app/head.tsx
@@ -15,22 +15,22 @@ export default function Head() {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@viistorrr" />
       <meta name="twitter:creator" content="@viistorrr" />
-      <meta name="twitter:title" content="Year Progress" />
-      <meta name="twitter:description" content="A simple year progress bar" />
+      <meta name="twitter:title" content="Progreso del Año" />
+      <meta name="twitter:description" content="Una simple barra de progreso que te muestra el porcentaje de avance del año" />
       <meta
         name="twitter:image"
         content="https://www.yearprogress.info/assets/imgs/preview.png"
       />
 
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="Year Progress" />
-      <meta property="og:description" content="A simple year progress bar" />
+      <meta property="og:title" content="Progreso del Año" />
+      <meta property="og:description" content="Una simple barra de progreso que te muestra el porcentaje de avance del año" />
       <meta
         property="og:image"
         content="https://www.yearprogress.info/assets/imgs/preview.png"
       />
       <meta property="og:url" content="https://www.yearprogress.info/" />
-      <meta property="og:site_name" content="Year Progress" />
+      <meta property="og:site_name" content="Progreso del Año" />
       <meta property="og:locale" content="en_US" />
 
       <meta name="twitter:card" content="summary_large_image" />
@@ -39,7 +39,7 @@ export default function Head() {
       <meta name="twitter:title" content="Progreso del Año" />
       <meta
         name="twitter:description"
-        content="Una simple barra de progreso que te muestra el porcentaje de avance del año "
+        content="Una simple barra de progreso que te muestra el porcentaje de avance del año"
       />
       <meta
         name="twitter:image"
