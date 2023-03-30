411ad61b8bec5e4ff96fcf9f7dfb5db0e3268e82
viistorrr <victormeza41@gmail.com>
2023-03-30 06:08:03 -0500

    fixing mb

--- app/page.tsx
+++ app/page.tsx
@@ -59,8 +59,6 @@ export default async function Home() {
 
     return days;
   }
-
-  console.log("------",dateDiff(hoy, date2));
   
   return (
     <main className="flex flex-col w-full justify-center items-center h-screen bg-white text-slate-700">
@@ -73,7 +71,7 @@ export default async function Home() {
             Día {day_of_year} de {TOTAL_DAYS}
           </h1>
         </div>
-        <span className="mb-4 px-4 text-base font-bold bg-emerald-100 rounded-full border-2 border-emerald-400">{weekDays[weekDay]}</span>
+        <span className="px-4 text-base font-bold bg-emerald-100 rounded-full border-2 border-emerald-400">{weekDays[weekDay]}</span>
         <div className="w-full  pr-8 pb-8 pl-8">
         <div className="flex flex-col w-full items-center align-center justify-center border-2 rounded-lg border-slate-300 pt-2 pr-8 pb-8 pl-8 shadow-lg mt-4 hover:pr-6 hover:pr-b6 hover:pl-6 hover:shadow-xl">
             <h1 className="text-lg font-bold text-slate-700 py-2">
