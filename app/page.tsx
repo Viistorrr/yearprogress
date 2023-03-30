bff86c2f3e25e72db482bedf8df84e4c707a5a3a
viistorrr <victormeza41@gmail.com>
2023-03-30 06:03:20 -0500

    reordering labels

--- app/page.tsx
+++ app/page.tsx
@@ -48,37 +48,48 @@ export default async function Home() {
   
   const YearPercent = (day_of_year / TOTAL_DAYS) * 100;
   const currentWeekPercent = (weekDay / TOTAL_WEEK_DAYS) * 100;
+
+  const hoy = new Date();
+  const date2 = new Date("2023-12-31");
   
+  const dateDiff = (hoy: Date, date2: Date) => {
+    const diff = hoy.getTime() - date2.getTime();
+    
+    let days = Math.floor(diff / (1000 * 3600 * 24));
+
+    return days;
+  }
+
+  console.log("------",dateDiff(hoy, date2));
   
   return (
     <main className="flex flex-col w-full justify-center items-center h-screen bg-white text-slate-700">
       {month ? <div className="flex flex-col w-full md:w-1/2 lg:w-1/2 items-center justify-center">
-        <Clock />
-        <h2 className="flex justify-center text-lg font-bold text-slate-700 py-2">
-          {localDateTime.slice(0, 10).replace(/,/g, "")}
-        </h2>
-        <h1 className="text-xl font-bold text-slate-700">
-          Día {day_of_year} de {TOTAL_DAYS}
-        </h1> 
+        <div className="py-2">
+          <h2 className="flex justify-center text-lg font-bold text-slate-700 py-2">
+            {localDateTime.slice(0, 10).replace(/,/g, "")}
+          </h2>
+          <h1 className="text-xl font-bold text-slate-700">
+            Día {day_of_year} de {TOTAL_DAYS}
+          </h1>
+        </div>
+        <span className="mb-4 px-4 text-base font-bold bg-emerald-100 rounded-full border-2 border-emerald-400">{weekDays[weekDay]}</span>
         <div className="w-full  pr-8 pb-8 pl-8">
-          <div className="flex flex-col w-full items-center align-center justify-center border-2 rounded-lg border-slate-300 pr-8 pb-8 pl-8 shadow-lg mt-4 hover:pr-6 hover:pr-b6 hover:pl-6 hover:shadow-xl">
-            <h1 className="font-bold py-4">
-              Semana {week_number} de {TOTAL_WEEKS}
+        <div className="flex flex-col w-full items-center align-center justify-center border-2 rounded-lg border-slate-300 pt-2 pr-8 pb-8 pl-8 shadow-lg mt-4 hover:pr-6 hover:pr-b6 hover:pl-6 hover:shadow-xl">
+            <h1 className="text-lg font-bold text-slate-700 py-2">
+              {getCurrentYear()}
             </h1>
-            <span className="mb-4 px-4 text-base font-bold bg-emerald-100 rounded-full border-2 border-emerald-400">{weekDays[weekDay]}</span>
             <div className="flex flex-row w-full items-center align-center">
               <div className="flex items-center w-11/12 bg-slate-100 rounded-full h-4">
                 <div
-                    className={`flex items-center justify-center py-2 ${getColor(
-                      currentWeekPercent
-                    )} h-4 rounded-full`}
-                  style={{
-                    width: currentWeekPercent.toString() + "%"
-                  }}
+                  className={`flex items-center justify-center py-2 ${getColor(
+                    YearPercent
+                  )} h-4 rounded-full`}
+                  style={{ width: YearPercent.toString() + "%" }}
                 ></div>
               </div>
               <h1 className="w-1/12 pl-2 pr-8 font-bold items-center text-sm text-sky-900">
-              {currentWeekPercent.toFixed(0)}%
+                {YearPercent.toFixed(0)}%
               </h1>
             </div>
           </div>
@@ -103,25 +114,28 @@ export default async function Home() {
               </h1>
             </div>
           </div>
-
-          <div className="flex flex-col w-full items-center align-center justify-center border-2 rounded-lg border-slate-300 pt-2 pr-8 pb-8 pl-8 shadow-lg mt-4 hover:pr-6 hover:pr-b6 hover:pl-6 hover:shadow-xl">
-            <h1 className="text-lg font-bold text-slate-700 py-2">
-              {getCurrentYear()}
+          <div className="flex flex-col w-full items-center align-center justify-center border-2 rounded-lg border-slate-300 pr-8 pb-8 pl-8 shadow-lg mt-4 hover:pr-6 hover:pr-b6 hover:pl-6 hover:shadow-xl">
+            <h1 className="font-bold py-4">
+              Semana {week_number} de {TOTAL_WEEKS}
             </h1>
+            
             <div className="flex flex-row w-full items-center align-center">
               <div className="flex items-center w-11/12 bg-slate-100 rounded-full h-4">
                 <div
-                  className={`flex items-center justify-center py-2 ${getColor(
-                    YearPercent
-                  )} h-4 rounded-full`}
-                  style={{ width: YearPercent.toString() + "%" }}
+                    className={`flex items-center justify-center py-2 ${getColor(
+                      currentWeekPercent
+                    )} h-4 rounded-full`}
+                  style={{
+                    width: currentWeekPercent.toString() + "%"
+                  }}
                 ></div>
               </div>
               <h1 className="w-1/12 pl-2 pr-8 font-bold items-center text-sm text-sky-900">
-                {YearPercent.toFixed(0)}%
+              {currentWeekPercent.toFixed(0)}%
               </h1>
             </div>
           </div>
+          
         </div>
       </div> : <Error />}
       <a
