34f081f87281d62f39a98b19ed2f1a1b87f398a4
viistorrr <victormeza41@gmail.com>
2023-07-10 05:28:37 -0500

    dump commit

--- app/page.tsx
+++ app/page.tsx
@@ -44,7 +44,6 @@ export default async function Home() {
   let  weekNumber = Math.ceil((pastDaysOfYear + firstDayOfYear.getDay()) / 7);
   if(dayOfWeekd == 7) weekNumber = weekNumber - 1
   const YearPercent = (dayOfYear / TOTAL_DAYS) * 100;
-  
 
   return (
     <main className="flex flex-col w-full justify-center items-center h-screen bg-white text-slate-700">
