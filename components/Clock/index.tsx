37752b0f37b7208186f4667f93666f11549bf056
viistorrr <victormeza41@gmail.com>
2023-07-11 10:23:43 -0500

    new bike gif

--- components/Clock/index.tsx
+++ components/Clock/index.tsx
@@ -15,14 +15,13 @@ const Clock = () => {
   }, []);
   return (
     <div className="flex items-center md:flex-col">
-      <iframe src="https://giphy.com/embed/tn8zWeNYA73G0" width="80" height="80" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/transparent-gallery-clock-tn8zWeNYA73G0"></a></p>
       <div className="pl-2">
         <h3 className="text-sm font-bold text-slate-700 pt-2">
           {date.toLocaleTimeString().toUpperCase()}
         </h3>
         <span className="text-sm font-bold text-slate-700 pt-2">Hora Colombia</span>
       </div>
-      
+      <iframe src="https://giphy.com/embed/3o7btQVgcavX2Ouaje" width="100" height="100" className="giphy-embed pl-4" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/animation-art-happy-3o7btQVgcavX2Ouaje"></a></p>
     </div>
   );
 };
