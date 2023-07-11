3700f5a7712ec9f8dca414c5c5ddab83760f26da
viistorrr <victormeza41@gmail.com>
2023-07-10 19:09:01 -0500

    gif added

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,7 @@ export default async function Home() {
         <span className="px-4 text-base font-bold bg-emerald-100 rounded-full border-2 border-emerald-400">{dayOfWeek}</span>
         
         
-        <div className="tenor-gif-embed" data-postid="11256412" data-share-method="host" data-aspect-ratio="1" data-width="100%"><a href="https://tenor.com/view/time-clock-gif-11256412">Time Clock GIF</a>from <a href="https://tenor.com/search/time-gifs">Time GIFs</a></div> <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
+        <iframe src="https://giphy.com/embed/tn8zWeNYA73G0" width="120" height="120" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/transparent-gallery-clock-tn8zWeNYA73G0"></a></p>
         <div className="w-full  pr-8 pb-8 pl-8">
         <div className="flex flex-col w-full items-center align-center justify-center border-2 rounded-lg border-slate-300 pt-2 pr-8 pb-8 pl-8 shadow-lg mt-4 hover:pr-6 hover:pr-b6 hover:pl-6 hover:shadow-xl">
             <h1 className="text-lg font-bold text-slate-700 py-2">
