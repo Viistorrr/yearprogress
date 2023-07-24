8ad78eeccd58261cf785c551ae64921373db5031
viistorrr <victormeza41@gmail.com>
2023-07-24 07:06:00 -0500

    fixing gif

--- components/Clock/index.tsx
+++ components/Clock/index.tsx
@@ -15,7 +15,7 @@ const Clock = () => {
   }, []);
   return (
     <div className="flex flex-col items-center">
-      <iframe src="https://giphy.com/embed/7Z8QOXkz3USMDb9NZH" width="60" height="80" className="giphy-embed rounded-xl" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/poop-toilet-poo-7Z8QOXkz3USMDb9NZH"></a></p>
+      <iframe src="https://giphy.com/embed/7Z8QOXkz3USMDb9NZH" width="60" height="80" className="giphy-embed rounded-lg" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/poop-toilet-poo-7Z8QOXkz3USMDb9NZH"></a></p>
       <h3 className="text-xs font-bold text-slate-700 pt-2 justify-center">
         {date.toLocaleTimeString().toUpperCase()} Hora Colombia
       </h3>
