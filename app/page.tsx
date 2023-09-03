8c6bd40db344d95c7c67acae660fc7227c92f88e
Víctor Meza <victormeza41@gmail.com>
2023-09-03 05:50:54 -0500

    dump commit

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import MonthInfo from "@components/MonthInfo";
 import YearInfo from "@components/YearInfo";
 import { updateData, scheduleNextUpdate } from "@utils/dateinfo";
 
+
 import {
   weekDays,
   months,
