3b4329902143b8c4eb48bb3fc9e24702e6caf639
Víctor Meza <victormeza41@gmail.com>
2025-08-04 10:47:22 -0500

    update url

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -12,16 +12,7 @@ export const Footer = () => (
                 className="text-sky-500 hover:text-sky-600 m-6"
                 >
                 <Instagram />
-            </a>
-            <a
-                key="Twitter"
-                href="https://twitter.com/intent/tweet?text=https://www.progresodelano.info/ @solotiips"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-sky-600"
-                >
-                <Twitter />
-            </a>
+            </a>            
             <a
                 key="Burger"
                 href="https://www.buymeacoffee.com/viistorrr"
@@ -36,7 +27,7 @@ export const Footer = () => (
         <div className="flex-col items-center justify-center text-sm text-sky-900 hover:text-sky-700 py-2">
             <i>
                 <a
-                    href="https://viistorrr.com/?utm_source=progresodelano&utm_medium=web"
+                    href="https://viistorrr.vercel.app"
                     target="_blank"
                     rel="noopener noreferrer"
                     >
